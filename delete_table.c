fc6133285a7c6caa40c7d1d739fc2ce7488addc2
MKekkar <mkekkar@ucsd.edu>
2024-08-23 12:06:41 -0700

    init starter code

---
+++ delete_table.c
@@ -0,0 +1,20 @@
+#include "Version.h"
+#if !defined(MYMAIN)
+#warning("using solution delete_table")
+#else
+#warning("using my delete_table.c")
+
+#include <stdlib.h>
+#include "node.h"
+
+void delete_table(node **htable, unsigned long tabsz) {
+	// TODO: walk through the chains in table
+	//
+	// TODO: free all the memory associated to each node in each chain
+	//
+	// TODO: free the entire table
+
+	(void) htable; // suppress unused variable warnings
+	(void) tabsz; // TODO: delete these once you start implementation
+}
+#endif
