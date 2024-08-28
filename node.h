8e8d12833fa256ccc49d4a6400bf589672b5171e
MKekkar <mkekkar@ucsd.edu>
2024-08-28 03:00:29 -0700

    clean up

--- node.h
+++ node.h
@@ -19,11 +19,12 @@ extern void delete_table(node **htable, unsigned long tabsz);
 extern void print_info(node **htable, unsigned long tabsz);
 #else
 .equ	NULL, 0
-//.equ	NODE_STRUCT_SIZE, __fill in__
-//.equ	NODE_ID_OFFSET, __fill in__
-//.equ	NODE_HEIGHT_OFFSET, __fill in with relative offset__
-//.equ	NODE_XCOORD_OFFSET, __fill in with relative offset__
-//.equ	NODE_YCOORD_OFFSET, __fill in with relative offset__
-//.equ	NODE_NEXT_OFFSET, __fill in with relative offset__
+//.equ	NODE_ID_OFFSET,     __fill in__
+//.equ	NODE_HEIGHT_OFFSET, __fill in with relative calculation__
+//.equ	NODE_XCOORD_OFFSET, __fill in with relative calculation__
+//.equ	NODE_YCOORD_OFFSET, __fill in with relative calculation__
+//.equ	NODE_NEXT_OFFSET,   __fill in with relative calculation__
+//.equ	NODE_PAD_OFFSET,    __fill in with relative calculation__
+//.equ	NODE_STRUCT_SIZE,   __fill in with relative calculation__
 #endif
 #endif
